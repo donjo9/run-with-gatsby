6873f80cb663e8af58205b95f83a924051b6f201
Johnni Mortensen <jm@lscontrol.dk>
2020-03-04 08:35:18 +0100

    add new favicon and change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Run With Johnni`,
+        short_name: `RunWith`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/run-icon.png`, // This path is relative to the root of the site.
       },
     },
     // Simple config, passing URL
